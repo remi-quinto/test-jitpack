b6a33398569458f44de2733064ad743b7ac26e6b
Remi Quinto <remi.quinto@gmail.com>
2022-04-11 22:33:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android_java_embedded_demo_app/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.android_java_embedded_demo_app;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.mylibrary.SdkActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+//    public String roomUrlString = "https://salesdemo.whereby.com/04cb2d34-6a34-4bd1-b1da-e1f89716b56c"; // Replace by your own
+    public String roomUrlString = "https://team.whereby.com/remi-quinto";
+//    private String roomParameters = "?needancestor&skipMediaPermissionPrompt";
+
+//    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, SdkActivity.class);
+        intent.putExtra("roomUrl", roomUrlString);
+        startActivity(intent);
+
+    }
+}
---
+++ mylibrary/src/main/java/com/example/mylibrary/CustomWebChromeClient.java
@@ -0,0 +1,27 @@
+package com.example.mylibrary;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+
+public class CustomWebChromeClient extends WebChromeClient {
+    private Activity activity;
+
+    public CustomWebChromeClient(Activity parentActivity) {
+        super();
+        activity = parentActivity;
+    }
+
+    @Override
+    public void onPermissionRequest(final PermissionRequest request) {
+        activity.runOnUiThread(new Runnable() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void run() {
+                request.grant(request.getResources());
+            }
+        });
+    }
+}
