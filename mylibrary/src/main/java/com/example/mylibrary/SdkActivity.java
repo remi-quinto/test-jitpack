ee6102d483e88882b7ecfdc4dae8cb39deb500fe
Remi Quinto <remi.quinto@gmail.com>
2022-06-14 20:40:50 +0200

    Fix static method 2

--- mylibrary/src/main/java/com/example/mylibrary/SdkActivity.java
+++ mylibrary/src/main/java/com/example/mylibrary/SdkActivity.java
@@ -24,7 +24,7 @@ public class SdkActivity extends AppCompatActivity {
     public static void testStaticMethod() {
         ;
     }
-    
+
     public String roomUrlString = "" ;//https://salesdemo.whereby.com/04cb2d34-6a34-4bd1-b1da-e1f89716b56c"; // Replace by your own
     //    https://team.whereby.com/remi-quinto?needancestor
     private String roomParameters = "?needancestor&skipMediaPermissionPrompt";
