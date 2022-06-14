79d703bcce0ead7f78a962a734d918f3a0cda858
Remi Quinto <remi.quinto@gmail.com>
2022-06-14 21:03:20 +0200

    Delete SdkActivity

--- app/src/main/java/com/example/android_java_embedded_demo_app/MainActivity.java
+++ app/src/main/java/com/example/android_java_embedded_demo_app/MainActivity.java
@@ -1,20 +1,9 @@
 package com.example.android_java_embedded_demo_app;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import android.Manifest;
+
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
-import com.example.mylibrary.SdkActivity;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
