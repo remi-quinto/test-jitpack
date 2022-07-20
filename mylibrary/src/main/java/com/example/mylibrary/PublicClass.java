4bf60246c9adb63e5a101972c8712eceecd3b6f1
Remi Quinto <remi.quinto@gmail.com>
2022-07-20 17:58:03 +0200

    0.2.12

--- mylibrary/src/main/java/com/example/mylibrary/PublicClass.java
+++ mylibrary/src/main/java/com/example/mylibrary/PublicClass.java
@@ -4,6 +4,9 @@ public class PublicClass {
     public String publicProperty;
     private String privateProperty;
 
+    /**
+     * This is public method
+     */
     public void publicMethod() {}
     private void privateMethod() {}
 }
