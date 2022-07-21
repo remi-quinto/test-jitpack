45cebb26da6c64c6c988ac728f4106d94b1b5fd5
Remi Quinto <remi.quinto@gmail.com>
2022-07-21 11:16:51 +0200

    0.2.16

--- app/src/main/java/com/example/android_java_embedded_demo_app/MainActivity.java
+++ app/src/main/java/com/example/android_java_embedded_demo_app/MainActivity.java
@@ -16,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
 
         PublicClass publicClass = new PublicClass();
         publicClass.publicProperty = "";
+        publicClass.publicMethod();
     }
 }
